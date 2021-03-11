b7460d7a3ff857fa1d75a3cd3da55365d5f2ed93
Bilal Uddin Khan <53609835+Mbialuddinkhan@users.noreply.github.com>
2021-03-11 21:49:14 +0500

    Add files via upload

---
+++ Data Science Libraries.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Mining libraries
+
+# scrapy
+
+# BeautifulSoup
+
+# # Data Processing and Modeling libraries
+
+# In[6]:
+
+
+import numpy as np
+
+
+# In[7]:
+
+
+import pandas as pd
+
+
+# SciPy
+
+# keras
+
+# SciKit-Learn
+
+# PyTorch
+
+# TensorFlow
+
+# XGBoost
+
+# # Data Visualization libraries
+
+# Matplotlib
+
+# Seaborn
+
+# Bokeh
+
+# Plotly
+
+# pydot
+
+# In[ ]:
+
+
+
+
